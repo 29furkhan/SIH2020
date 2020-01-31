f165cbc95b53518ca972aa890ec58882e752aedc
Prasad <nagthanep@gmail.com>
2020-01-31 11:27:51 +0530

    student pro created

---
+++ new.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import render_template,request,redirect,url_for
+app = Flask(__name__)
+    
+
+@app.route('/',methods=['GET','POST'])
+def hello_world():
+    return render_template('input.html')
+
+@app.route('/alumni/dashboard')
+def dashboardalumni():
+    return render_template("pages/alumni/dashboardalumni.html")
+
+
+@app.route('/alumni/profile')
+def Aluminiprofile():
+    return render_template("pages/alumni/profile.html")
+
+@app.route('/alumni/www.facebook.com')
+def facebook():
+    return redirect("https://www.facebook.com",code=302)
+
+@app.route('/alumni/www.twitter.com')
+def twitter():
+    return redirect("https://www.twitter.com",code=302)
+
+@app.route('/alumni/www.google.com')
+def google():
+    return redirect("https://www.google.com",code=302)
+
+@app.route('/student/profile')
+def Studentprofile():
+    return render_template("pages/student/studentprofile.html")
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
