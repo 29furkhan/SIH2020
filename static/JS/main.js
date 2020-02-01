738d4e218e1bd2a2fc6e56c62e6f345c8c03a37b
bhakti <bhaktipatil246@gmail.com>
2020-02-01 09:29:41 +0530

    directordashboard

---
+++ static/JS/main.js
@@ -0,0 +1,22 @@
+function getUserType(type){
+    type = document.getElementById(type).value;
+
+    if(type=="alumni" || type=="student")
+    {
+        document.getElementById('PRN_NO').style.display="table-row";
+        if(type=="alumni" || type=="student")
+        {
+            document.getElementById('collegeselecttr').style.display="table-row";
+            document.getElementById('dircollegeselect').style.display="none";
+        }
+        else if(type="DIR"){
+            document.getElementById('dircollegeselect').style.display="table-row";
+            document.getElementById('collegeselecttr').style.display="none";
+        }
+        else if(type="DHE"){
+            document.getElementById('userid').style.display="table-row";
+            document.getElementById('password').style.display="table-row";
+        }
+    }
+
+}
